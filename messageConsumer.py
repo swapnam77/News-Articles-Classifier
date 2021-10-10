d6901809a8771bbe8b44bc8c31dd2de5d84455cb
swapnam77 <86954031+swapnam77@users.noreply.github.com>
2021-10-10 17:44:43 +0530

    Add files via upload

---
+++ messageConsumer.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from kafka import KafkaConsumer
+from json import loads
+import json
+import time
+from pymongo import MongoClient
+
+consumer = KafkaConsumer('news', bootstrap_servers=['localhost:9092'])
+client = MongoClient("mongodb://localhost:27017/")
+mydb = client["Capstone"]
+mycoll = mydb["newsCollection"]
+
+for message in consumer:
+    record = json.loads(message.value)
+    news_record=json.loads(record)
+    mycoll.insert_one(news_record)
+    print (news_record)
+
