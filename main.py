324637e543b3388454b427392d434497f7aa6bbe
swapnam77 <86954031+swapnam77@users.noreply.github.com>
2021-10-24 22:51:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,63 @@
-import newsrapidAPI
-import newsAPIService
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from ml_utils import load_model, predict, retrain
+from typing import List
+import datetime;
 
-if __name__ == '__main__':
-    title=["Finance","Sports","Politics","Religion","Education","Entertainment","Health","Business","cryptocurrency"]
-    for i in title:
-        print(i)
-        newsrapidAPI.getNews(i)
-        newsAPIService.getNews(i)
+# defining the main app
+app = FastAPI(title="News Predictor", docs_url="/")
+
+# calling the load_model during startup.
+# this will train the model and keep it loaded for prediction.
+app.add_event_handler("startup", load_model)
+
+# class which is expected in the payload
+class QueryIn(BaseModel):
+    title: str
+    summary: str
 
 
+# class which is returned in the response
+class QueryOut(BaseModel):
+    category: str
+
+# class which is expected in the payload while re-training
+class FeedbackIn(BaseModel):
+    title: str
+    summary: str
+    category: str
+
+# Route definitions
+@app.get("/ping")
+# Healthcheck route to ensure that the API is up and running
+def ping():
+    return {"ping": "pong"}
+
+
+@app.post("/predict_category", response_model=QueryOut, status_code=200)
+# Route to do the prediction using the ML model defined.
+# Payload: QueryIn containing the parameters
+# Response: QueryOut containing the flower_class predicted (200)
+def predict_category(query_data: QueryIn):
+    #input=list(query_data.dict().values())
+    #print (input[0])
+    #print (type(input))
+    output = {"category": predict(query_data)}
+    return output
+
+@app.post("/feedback_loop", status_code=200)
+# Route to further train the model based on user input in form of feedback loop
+# Payload: FeedbackIn containing the parameters and correct flower class
+# Response: Dict with detail confirming success (200)
+def feedback_loop(data: FeedbackIn):
+    print(data)
+    print(type(data))
+    retrain(data)
+    return {"detail": "Feedback loop successful"}
+
+
+# Main function to start the app when main.py is called
+if __name__ == "__main__":
+    # Uvicorn is used to run the server and listen for incoming API requests on 0.0.0.0:8888
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
